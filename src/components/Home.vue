733a01e992a27a91479a660f39a96fad36e6a53d
ford-zii <shissanupong@outlook.com>
2020-01-22 16:33:57 +0700

    add source file .

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,92 @@
 <template>
-    $END$
+    <div>
+        <v-app-bar
+                color="deep-purple accent-4"
+                dense
+                dark
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Page title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-menu
+                    left
+                    bottom
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item
+                            v-for="n in 5"
+                            :key="n"
+                            @click="() => {}"
+                    >
+                        <v-list-item-title>Option {{ n }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+
+        <v-footer
+                dark
+                padless
+        >
+            <v-card
+                    flat
+                    tile
+                    class="indigo lighten-1 white--text text-center"
+            >
+                <v-card-text>
+                    <v-btn
+                            v-for="icon in icons"
+                            :key="icon"
+                            class="mx-4 white--text"
+                            icon
+                    >
+                        <v-icon size="24px">{{ icon }}</v-icon>
+                    </v-btn>
+                </v-card-text>
+
+                <v-card-text class="white--text pt-0">
+                    Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-text class="white--text">
+                    {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+                </v-card-text>
+            </v-card>
+        </v-footer>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data: () => ({
+            icons: [
+                'fab fa-facebook',
+                'fab fa-twitter',
+                'fab fa-google-plus',
+                'fab fa-linkedin',
+                'fab fa-instagram',
+            ],
+        }),
     }
 </script>
 
